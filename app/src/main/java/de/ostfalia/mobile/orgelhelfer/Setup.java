fd873b47a784d1f65552a41a9ca5f505a79c313f
Frede9 <frederik-riess@web.de>
2018-05-20 21:13:09 +0200

    Added Start Activity Changed Start Activity to default Added Playlist Added Background

--- app/src/main/java/de/ostfalia/mobile/orgelhelfer/Setup.java
+++ app/src/main/java/de/ostfalia/mobile/orgelhelfer/Setup.java
@@ -3,13 +3,12 @@ package de.ostfalia.mobile.orgelhelfer;
 import android.app.ListActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-
-
-public class Setup extends ListActivity {
+public class Setup extends AppCompatActivity {
 
     private EditText et_dateipfad;
     private ImageButton btn_dateipfad;
@@ -22,12 +21,7 @@ public class Setup extends ListActivity {
         et_dateipfad = (findViewById(R.id.dateipfad));
         btn_dateipfad = (findViewById(R.id.ib_dateipfad));
 
-        btn_dateipfad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
 
 
 
